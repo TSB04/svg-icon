80c7bf1523669039c4ea7b65e1fef9b67b4886fc
Thierno Sadou BARRY <twizfouiny@gmail.com>
2025-04-22 12:19:59 +0200

    Add some more icons from my icons.js

---
+++ src/icons/Error.ts
@@ -0,0 +1,13 @@
+import { IconPaths, IconVariant } from "../types/icons";
+
+const errorIconPaths: IconPaths = {
+  default:
+    "M12 2 C6.477 2 2 6.477 2 12 C2 17.523 6.477 22 12 22 C17.523 22 22 17.523 22 12 C22 6.477 17.523 2 12 2 Z M14.53 15.53 C14.823 15.237 14.823 14.762 14.53 14.47 L12 12 L14.53 9.53 C14.823 9.237 14.823 8.762 14.53 8.47 C14.237 8.177 13.762 8.177 13.47 8.47 L12 9.94 L10.53 8.47 C10.237 8.177 9.762 8.177 9.47 8.47 C9.177 8.762 9.177 9.237 9.47 9.53 L12 12 L9.47 14.47 C9.177 14.762 9.177 15.237 9.47 15.53 C9.762 15.823 10.237 15.823 10.53 15.53 L12 13.06 L13.47 15.53 C13.762 15.823 14.237 15.823 14.53 15.53 Z",
+  bold: "",
+  thin: "",
+  filled: "",
+};
+
+export const getErrorIconPath = (variant: IconVariant = "default"): string => {
+  return errorIconPaths[variant] ?? errorIconPaths.default;
+};
---
+++ src/icons/brand/Twitter.ts
@@ -0,0 +1,12 @@
+import { IconPaths, IconVariant } from "../../types/icons";
+
+const twitterIconPaths: IconPaths = {
+  default: "M13.6894 18.5L9.90566 13.1069L5.16898 18.5H3.16504L9.01663 11.8394L3.16504 3.5H8.31001L11.8761 8.58297L16.3443 3.5H18.3482L12.7681 9.85216L18.8343 18.5H13.6894ZM15.921 16.9795H14.5719L6.03435 5.02047H7.38366L10.803 9.809L11.3943 10.6399L15.921 16.9795Z",
+  bold: "",
+  thin: "",
+  filled: "",
+};
+
+export const getTwitterIconPath = (variant: IconVariant = "default"): string => {
+  return twitterIconPaths[variant] ?? twitterIconPaths.default;
+};

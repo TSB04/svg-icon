1c3fecfeff77f01c5b95c3cfa9bd9061df41f2c1
Thierno Sadou BARRY <twizfouiny@gmail.com>
2025-04-29 10:40:14 +0200

    Updated build and export logic

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@
 export { IconWrapper } from "./components/IconWrapper";
 export { IconVariant } from "./types/icons";
 
-export * from "./icons/iconPath/brand";
-export * from "./icons/iconPath/common";
-
 export * from "./web-component";
 
 // framework-specific
-export * from "./icons/jsx/brand";
 export * from "./icons/jsx/common"; // JSX icon components
